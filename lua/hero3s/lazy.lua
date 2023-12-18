ccf9fc900a2961b56e73aa7ceaf7c2af92caa65f
HeRo3Sm <hungid6a1@gmail.com>
2023-12-18 15:38:26 +0700

    remove some line in lazy vim

--- lua/hero3s/lazy.lua
+++ lua/hero3s/lazy.lua
@@ -43,10 +43,8 @@ require("lazy").setup({
 			"tpope/vim-fugitive",
 		},
 		-- LSP
-		--- Uncomment these if you want to manage LSP servers from neovim
 		{ "williamboman/mason.nvim" },
 		{ "williamboman/mason-lspconfig.nvim" },
-
 		{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
 		{ "neovim/nvim-lspconfig" },
 		{ "hrsh7th/cmp-nvim-lsp" },

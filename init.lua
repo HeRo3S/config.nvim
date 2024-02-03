0f23a5e0b48103d3fe78fb5512335c3d0863b498
HeRo3S <hungid6a1@gmail.com>
2024-02-03 21:30:03 +0700

    importing local windows file

---
+++ init.lua
@@ -0,0 +1,28 @@
+-- Remaps
+require('hero3s.set')
+require('hero3s.remap')
+
+-- Package manager
+require('hero3s.lazy')
+-- Colorscheme has to wait for package manager to be setup 
+require('plugins.colorscheme')
+
+-- Must-have
+require('plugins.telescope')
+require('plugins.treesitter')
+require('plugins.lsp')
+
+-- Versions control
+require('plugins.undotree')
+require('plugins.fugitive')
+
+-- QoL plugins
+require('plugins.autopair')
+require('plugins.nvim-comment')
+require('plugins.todo-comments')
+require('plugins.efmls-configs-nvim')
+require('plugins.which-key')
+require('plugins.nvim-surround')
+
+-- Markdown
+require('plugins.glow')
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,78 @@
+local lsp = require("lsp-zero").preset({
+	name = "minimal",
+	set_lsp_keymaps = true,
+	manage_nvim_cmp = true,
+	suggest_lsp_servers = false,
+})
+
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"tsserver",
+		"lua_ls",
+		"pyright",
+		"rust_analyzer",
+		"tailwindcss",
+		"jsonls",
+		"cssls",
+		"bashls",
+		"clangd",
+		"efm",
+        "texlab"
+	},
+	handlers = {
+		lsp.default_setup,
+	},
+})
+
+-- Some custom keymap for LSP
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+	mapping = cmp.mapping.preset.insert({
+		["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+		["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
+		["<C-Space>"] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm({select = true}),
+		["<Tab>"] = nil,
+		["<S-Tab>"] = nil,
+	}),
+})
+
+lsp.on_attach(function(client, bufnr)
+	local opts = { buffer = bufnr, remap = false }
+
+	vim.keymap.set("n", "gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<leader>vws", function()
+		vim.lsp.buf.workspace_symbol()
+	end, opts)
+	vim.keymap.set("n", "<leader>vd", function()
+		vim.diagnostic.open_float()
+	end, opts)
+	vim.keymap.set("n", "[d", function()
+		vim.diagnostic.goto_next()
+	end, opts)
+	vim.keymap.set("n", "]d", function()
+		vim.diagnostic.goto_prev()
+	end, opts)
+	vim.keymap.set("n", "<leader>vca", function()
+		vim.lsp.buf.code_action()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrr", function()
+		vim.lsp.buf.references()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrn", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	vim.keymap.set("i", "<C-h>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
+end)
+

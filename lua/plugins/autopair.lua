8da719dcf058eff4bc72b74496e0ed1641afc95c
HeRo3S <hungid6a1@gmail.com>
2024-06-18 10:16:32 +0700

    fixed autopairs plugin not loaded

--- lua/plugins/autopair.lua
+++ lua/plugins/autopair.lua
@@ -3,9 +3,20 @@ require("nvim-autopairs").setup({
 	check_ts = true,
 })
 
-require("nvim-treesitter.configs").setup({
-	autotag = {
-		enable = true,
-	},
+require('nvim-ts-autotag').setup({
+  opts = {
+    -- Defaults
+    enable_close = true, -- Auto close tags
+    enable_rename = true, -- Auto rename pairs of tags
+    enable_close_on_slash = false -- Auto close on trailing </
+  },
+  -- Also override individual filetype configs, these take priority.
+  -- Empty by default, useful if one of the "opts" global settings
+  -- doesn't work well in a specific filetype
+  per_filetype = {
+    ["html"] = {
+      enable_close = false
+    }
+  }
 })
 

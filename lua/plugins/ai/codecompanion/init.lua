465d5be2f7d4ff61a5856c14b4f902ffc1a24db2
HeRo3S <hungid6a1@gmail.com>
2025-04-04 09:30:50 +0700

    add telescope support to codecompanion

--- lua/plugins/ai/codecompanion/init.lua
+++ lua/plugins/ai/codecompanion/init.lua
@@ -4,44 +4,75 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
 	},
-	opts = {
-		adapters = {
-			gemini = function()
-				return require("codecompanion.adapters").extend("gemini", {
-					schema = {
-						model = {
-							default = "gemini-2.5-pro-exp-03-25",
+	opts = function()
+		return {
+			adapters = {
+				gemini = function()
+					return require("codecompanion.adapters").extend("gemini", {
+						schema = {
+							model = {
+								default = "gemini-2.5-pro-exp-03-25",
+							},
+						},
+					})
+				end,
+			},
+			strategies = {
+				chat = {
+					adapter = "gemini",
+					slash_commands = {
+						codebase = require("vectorcode.integrations").codecompanion.chat.make_slash_command(),
+						file = {
+							opts = {
+								provider = "telescope", -- Other options include 'default', 'mini_pick', 'fzf_lua', snacks
+								contains_code = true,
+							},
+						},
+						buffer = {
+							opts = {
+								provider = "telescope", -- Other options include 'default', 'mini_pick', 'fzf_lua', snacks
+								contains_code = true,
+							},
+						},
+						symbols = {
+							opts = {
+								provider = "telescope", -- Other options include 'default', 'mini_pick', 'fzf_lua', snacks
+								contains_code = true,
+							},
 						},
 					},
-				})
-			end,
-		},
-		strategies = {
-			chat = {
-				adapter = "gemini",
-				slash_commands = {
-					codebase = require("vectorcode.integrations").codecompanion.chat.make_slash_command(),
+					tools = {
+						vectorcode = {
+							description = "Run VectorCode to retrieve the project context.",
+							callback = require("vectorcode.integrations").codecompanion.chat.make_tool(),
+						},
+					},
 				},
-				tools = {
-					vectorcode = {
-						description = "Run VectorCode to retrieve the project context.",
-						callback = require("vectorcode.integrations").codecompanion.chat.make_tool(),
+				inline = {
+					adapter = "gemini",
+				},
+			},
+			display = {
+				chat = {
+					window = {
+						position = "right",
 					},
 				},
 			},
-			inline = {
-				adapter = "gemini",
-			},
-		},
-		display = {
-			chat = {
-				window = {
-					position = "right",
-				},
-			},
-		},
-	},
+		}
+	end,
 	init = function()
 		require("plugins.ai.codecompanion.lualine"):init()
+		vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+		vim.keymap.set(
+			{ "n", "v" },
+			"<LocalLeader>a",
+			"<cmd>CodeCompanionChat Toggle<cr>",
+			{ noremap = true, silent = true }
+		)
+		vim.keymap.set("v", "ca", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+		-- Expand 'cc' into 'CodeCompanion' in the command line
+		vim.cmd([[cab cc CodeCompanion]])
 	end,
 }

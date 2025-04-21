197b30828632b33cdf4bc36a7fe673f06e262d14
HeRo3S <hungid6a1@gmail.com>
2025-04-21 10:51:26 +0700

    add flash.nvim because it's so cool

--- lua/plugins/qol.lua
+++ lua/plugins/qol.lua
@@ -8,6 +8,20 @@ return {
 		},
 	},
 	{ "folke/todo-comments.nvim", opts = {} },
+	{
+		"folke/flash.nvim",
+        event = "VeryLazy",
+		opts = {},
+		keys = {
+            -- stylua: ignore start
+		    { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "<leader>S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+            -- stylua: ignore end
+		},
+	},
 	{ "terrortylor/nvim-comment" },
 	{ "windwp/nvim-autopairs", opts = {
 		disable_filetype = { "TelescopePrompt", "vim" },

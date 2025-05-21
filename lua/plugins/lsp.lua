6ebccbf8d89bb3df2e6e4eec8f21194a1a1b75a8
HeRo3S <hungid6a1@gmail.com>
2025-05-21 10:22:31 +0700

    [change, lsp]: remove abundant lsp handlers to compliance with nvim 0.11

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,18 +5,10 @@ return {
 			{ "saghen/blink.cmp", version = "*" },
 			{ "williamboman/mason.nvim" },
 			{ "williamboman/mason-lspconfig.nvim" },
-			{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
 			--Snippet
 			{ "L3MON4D3/LuaSnip" },
 		},
 		config = function()
-			local lsp = require("lsp-zero").preset({
-				name = "minimal",
-				set_lsp_keymaps = true,
-				manage_nvim_cmp = true,
-				suggest_lsp_servers = false,
-			})
-
 			require("mason").setup({})
 			require("mason-lspconfig").setup({
 				ensure_installed = {
@@ -32,7 +24,7 @@ return {
 					-- this first function is the "default handler"
 					-- it applies to every language server without a "custom handler"
 					function(server_name)
-						require("lspconfig")[server_name].setup({})
+						vim.lsp.enable(server_name)
 					end,
 				},
 			})
@@ -60,40 +52,24 @@ return {
 				},
 			})
 
-			lsp.on_attach(function(client, bufnr)
-				local opts = { buffer = bufnr, remap = false }
-
-				vim.keymap.set("n", "gd", function()
-					vim.lsp.buf.definition()
-				end, opts)
-				vim.keymap.set("n", "K", function()
-					vim.lsp.buf.hover()
-				end, opts)
-				vim.keymap.set("n", "<leader>vws", function()
-					vim.lsp.buf.workspace_symbol()
-				end, opts)
-				vim.keymap.set("n", "<leader>vd", function()
-					vim.diagnostic.open_float()
-				end, opts)
-				vim.keymap.set("n", "[d", function()
-					vim.diagnostic.goto_prev()
-				end, opts)
-				vim.keymap.set("n", "]d", function()
-					vim.diagnostic.goto_next()
-				end, opts)
-				vim.keymap.set("n", "<leader>vca", function()
-					vim.lsp.buf.code_action()
-				end, opts)
-				vim.keymap.set("n", "<leader>vrr", function()
-					vim.lsp.buf.references()
-				end, opts)
-				vim.keymap.set("n", "<leader>vrn", function()
-					vim.lsp.buf.rename()
-				end, opts)
-				vim.keymap.set("i", "<C-h>", function()
-					vim.lsp.buf.signature_help()
-				end, opts)
-			end)
+			vim.api.nvim_create_autocmd("LspAttach", {
+				desc = "LSP actions",
+				callback = function(event)
+					local opts = { buffer = event.buf }
+					vim.keymap.set("n", "gd", function()
+						vim.lsp.buf.definition()
+					end, opts)
+					vim.keymap.set("n", "<leader>vws", function()
+						vim.lsp.buf.workspace_symbol()
+					end, opts)
+					vim.keymap.set("n", "<leader>vd", function()
+						vim.diagnostic.open_float()
+					end, opts)
+					vim.keymap.set("i", "<C-h>", function()
+						vim.lsp.buf.signature_help()
+					end, opts)
+				end,
+			})
 		end,
 	},
 }

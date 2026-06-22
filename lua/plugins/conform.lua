b35ddee0827a641fae66d947460356561d46331a
HeRo3S <hungid6a1@gmail.com>
2026-06-22 13:50:53 +0700

    feat(conform): add vue formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -26,6 +26,7 @@ return {
 				typescript = { "prettierd", "prettier", stop_after_first = true },
 				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
 				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+				vue = { "prettierd", "prettier", stop_after_first = true },
 				css = { "prettierd", "prettier", stop_after_first = true },
 				scss = { "prettierd", "prettier", stop_after_first = true },
 				html = { "prettierd", "prettier", stop_after_first = true },
